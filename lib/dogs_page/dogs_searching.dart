e326f1d8991dddf360c70417f35019b38feabacf
elis-li <petrushina.2014@mail.ru>
2024-09-20 16:16:53 +0300

    UI

--- lib/dogs_page/dogs_searching.dart
+++ lib/dogs_page/dogs_searching.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class DogsSearchingPage extends StatelessWidget{
   const DogsSearchingPage({super.key});
@@ -13,7 +14,29 @@ class DogsSearchingPage extends StatelessWidget{
         elevation: 0,
         backgroundColor: Colors.brown,
       ),
-      body: Container(color: Colors.brown[300]),
+      body: Container(color: Colors.brown[300], child: const EnterBreed()),
+    );
+  }
+}
+class EnterBreed extends StatelessWidget{
+  const EnterBreed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10,
+            vertical: 20),
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter a dog breed to search'
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
